96f240f04b2ddd2fbd232da0501e7e8e6154b0ca
tinu bose <tinubose.mca2022@gmail.com>
2023-11-30 19:38:40 +0530

    added signup page

--- soln/index.php
+++ soln/index.php
@@ -42,8 +42,17 @@
 <div class="container" id="container">
     <div class="form-container sign-up-container">
         <form action="signup.php" method="post">
-            <!-- Your existing sign-up form -->
-            <!-- ... (existing sign-up form HTML) ... -->
+        <h1>Create Account</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" name="name"/>
+			<input type="email" placeholder="Email" name="email"/>
+			<input type="password" placeholder="Password" name="password"/>
+			<button>Sign Up</button>
         </form>
     </div>
     <div class="form-container sign-in-container">
